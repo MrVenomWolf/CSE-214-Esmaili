b87615a2e2ff27fc2fb1c720d7fec5b53610c6d1
MrVenomWolf <86920461+MrVenomWolf@users.noreply.github.com>
2022-08-23 15:46:00 -0400

    Delete ItemInfo.java

--- ItemInfo.java
+++
@@ -1,9 +0,0 @@
-//Anubrota Majumdar, 114586887, Recitation 30
- public class ItemInfo {
-	 String name;
-	 double price;
-	 String rfidTagNumber;
-	 String OriginalLocation;
-	 String CurrentLocation;
-	
-}
